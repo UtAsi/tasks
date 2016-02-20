346e8d36c32f715bc33f985343c060b8746b055a
asai <asai.nrv@gmail.com>
2016-02-21 02:05:43 +0900

    update

--- bin/build-css.sh
+++ bin/build-css.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm run css:sass && npm run css:prefix && npm run css:comb && npm run css:copy && npm run css:min
+npm run css:sass && npm run css:prefix && npm run css:comb && npm run css:copy && npm run css:min && npm run css:clean
